9f40cc5788d92af943ce437712cfc80237b26836
maslina3000 <andrey.worobyov46@gmail.com>
2019-04-06 20:16:38 +0300

    коммит пока работает

--- app/src/main/java/com/example/here/MainActivity.java
+++ app/src/main/java/com/example/here/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.here;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void newActivity(View view)
+    {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
     }
 
 
